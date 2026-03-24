fbc1dd7bca7044bda5bfca0b63813ab39712aefb
arnaud-devs <shemaarnaud69@gmail.com>
2026-03-24 14:54:02 +0200

    feat(auth-logo): replace text with logo image in AuthLogo component

--- components/auth/AuthLogo.tsx
+++ components/auth/AuthLogo.tsx
@@ -1,14 +1,13 @@
-import { Text, View } from "react-native";
+import { Image, View } from "react-native";
 
 export default function AuthLogo() {
   return (
     <View className="items-center mt-5 mb-10">
-      <Text className="text-5xl font-bold text-[#1363A5] -tracking-wider">
-        huza<Text className="text-[#F48C06]">Go</Text>
-      </Text>
-      <Text className="text-xxs text-[#1363A5] -mt-1">
-        Fast & Affordable Local Delivery
-      </Text>
+      <Image
+        source={require("@/assets/images/AuthLogo.png")}
+        style={{ width: 180, height: 100 }}
+        resizeMode="contain"
+      />
     </View>
   );
 }
