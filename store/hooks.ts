d96f259451268167346969761d5e572b572ceaff
arnaud-devs <shemaarnaud69@gmail.com>
2026-04-03 22:13:01 +0200

    refactor: remove user slice, store configuration, tailwind config, and TypeScript configuration files

--- store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { AppDispatch, RootState } from './store';
-
-// Use this typed useDispatch and useSelector throughout your app
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
